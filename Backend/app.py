3db6b14735b9294fef5083f577c674d983d23768
wacorado <58500299+wacorado@users.noreply.github.com>
2021-06-29 22:20:25 -0600

    Cuarto Comit
    Aqui ya envio data a flask en flask la interpreto y ya tengo las listas de los datos de los Primeros 3 reportes me faltan 2 reportes y solucionar el dato nuevo de stock en juegos.

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,8 @@
+from tkinter import Listbox
 from flask import *
 import xml.etree.ElementTree as ET
 from flask.wrappers import Request
+from jinja2.utils import evalcontextfunction
 from werkzeug.wrappers import Response
 
 strBkpReturn=''
@@ -17,16 +19,14 @@ def hello():
 @app.route('/clientes-xml', methods = ['POST'])
 def post_ClientesXML():
     global strBkpReturn
-    clientes=[]
-    # mejoresClientes=[]
-    # juegos=[]
-    # juegosVendidos=[]
+    listaclientes=[]
+    listaMejoresClientes=[]
+    listaJuegos=[]
+    listaJuegosMasVendidos=[]
     # cantidadGastada=0
     str_Archivo= request.data.decode('UTF-8')
     strBkpReturn=request.data.decode('UTF-8')
-    #print(str(str_Archivo))
     raiz = ET.fromstring(str_Archivo)
-    #root=tree.getroot()
     for elemento in raiz:
         print("-----------------------"+elemento.tag+"---------------------")
         if(elemento.tag=="Clientes"):
@@ -48,90 +48,175 @@ def post_ClientesXML():
                     elif(datosCliente.tag=="FechaPrimeraCompra"):
                         print(datosCliente.text)
                         fechaPrimeraCompra=datosCliente.text
-                clientes.append([nombre,apellido,edad,fechaCumple,fechaPrimeraCompra])
-                    
-                    
+                print("Cliente Guardado Correctamente")
+                listaclientes.append([nombre,apellido,edad,fechaCumple,fechaPrimeraCompra])
+        elif(elemento.tag=="MejoresClientes"):
+            for mejoresClientes in elemento:
+                print("-----------------"+mejoresClientes.tag+"------------------------")
+                for datosMejorCliente in mejoresClientes:
+                    if(datosMejorCliente.tag=="Nombre"):
+                        print(datosMejorCliente.text)
+                        nombreMejorCliente=datosMejorCliente.text
+                    elif(datosMejorCliente.tag=="FechaUltimaCompra"):
+                        print(datosMejorCliente.text)
+                        fechaUltimaCompraMejorCliente=datosMejorCliente.text
+                    elif(datosMejorCliente.tag=="CantidadComprada"):
+                        print(datosMejorCliente.text)
+                        cantidadComprada=datosMejorCliente.text
+                    elif(datosMejorCliente.tag=="CantidadGastada"):
+                        print(datosMejorCliente.text)
+                        cantidadGastada=datosMejorCliente.text
+                print("Mejor Cliente Guardado Exitosamente")
+                listaMejoresClientes.append([nombreMejorCliente,fechaUltimaCompraMejorCliente,cantidadComprada,cantidadGastada])
+        elif(elemento.tag=="Juegos"):
+            for juegos in elemento:
+                print("-----------------"+juegos.tag+"-------------------")
+                for datosJuego in juegos:
+                    if(datosJuego.tag=="Nombre"):
+                        print(datosJuego.text)
+                        nombreJuego=datosJuego.text
+                    elif(datosJuego.tag=="Plataforma"):
+                        print(datosJuego.text)
+                        plataformaJuego=datosJuego.text
+                    elif(datosJuego.tag=="AñoLanzamiento"):
+                        print(datosJuego.text)
+                        añoLanzamientoJuego=datosJuego.text
+                    elif(datosJuego.tag=="Clasificacion"):
+                        print(datosJuego.text)
+                        clacificacionJuego=datosJuego.text
+                print("Juego Registrado Exitosamente")
+                listaJuegos.append([nombreJuego,plataformaJuego,añoLanzamientoJuego,clacificacionJuego])
+        elif(elemento.tag=="JuegosMasVendidos"):
+            for juegosMasVendidos in elemento:
+                print("-----------------"+juegosMasVendidos.tag+"----------------")
+                for datoJuegoMasVendido in juegosMasVendidos:
+                    if(datoJuegoMasVendido.tag=="Nombre"):
+                        print(datoJuegoMasVendido.text)
+                        nombreJuegoMasVendido=datoJuegoMasVendido.text
+                    elif(datoJuegoMasVendido.tag=="FechaUltimaCompra"):
+                        print(datoJuegoMasVendido.text)
+                        fechaUitimaCompraJuegoMasVendido=datoJuegoMasVendido.text
+                    elif(datoJuegoMasVendido.tag=="CopiasVendidas"):
+                        print(datoJuegoMasVendido.text)
+                        copiasVendidasJuegosMasVendido=datoJuegoMasVendido.text
+                    elif(datoJuegoMasVendido.tag=="Stock"):
+                        print(datoJuegoMasVendido.tag)
+                        stockJuegoMasVendido=datoJuegoMasVendido.text
+                print("Juego Mas Vendido Registrado Exitosamente")
+                listaJuegosMasVendidos.append([nombreJuegoMasVendido,fechaUitimaCompraJuegoMasVendido,copiasVendidasJuegosMasVendido,stockJuegoMasVendido])
 
-    #for hijo in raiz:
-    #    print("------------hijo.tag ------------------")
-    #     if(hijo.tag()=="cliente")
-    #     #print("---------------------"+hijo.tag+"--------------------------")
-    #     if(hijo.tag == "cliente"):
-    #         for subhijo in hijo:
-    #             if(subhijo.tag=="nombre"):
-    #                 nombre=subhijo.text
-    #             elif(subhijo.tag=="apellido"):
-    #                 apellido=subhijo.text
-    #             elif(subhijo.tag=="edad"):
-    #                 edad=subhijo.text
-    #             elif(subhijo.tag=="fechaCumpleaños"):
-    #                 cumpleFecha=subhijo.text
-    #             elif(subhijo.tag=="cantidadGastada"):
-    #                 cantidadGastada=subhijo.text
-    #         clientes.append([nombre,apellido,edad,cumpleFecha,cantidadGastada])
-    #         print("Cliente Guardado Correctamente")
-        
-    #     elif(hijo.tag=="mejoresClientes"):
-    #         for subhijo in hijo:
-    #             if(subhijo.tag=="nombre"):
-    #                 nombre=subhijo.text
-    #             elif(subhijo.tag=="fechaUltimaCompra"):
-    #                 fechaUltimaC=subhijo.text
-    #             elif (subhijo.tag=="cantidadComprada"):
-    #                 cantComprada=subhijo.text
-    #             elif(subhijo.tag=="cantidadGastada"):
-    #                 cantidadGastada=subhijo.text
-    #         mejoresClientes.append([nombre, fechaUltimaC, cantComprada, cantidadGastada])
-    #         print("Mejor Cliente Guardado Correctamente")
-        
-    #     elif(hijo.tag=="juegos"):
-    #         for subhijo in hijo:
-    #             if(subhijo.tag=="nombre"):
-    #                 nombre=subhijo.text
-    #             elif(subhijo.tag=="plataforma"):
-    #                 plataforma=subhijo.text
-    #             elif(subhijo.tag=="añoLanzamiento"):
-    #                 yearLanzamiento=subhijo.text
-    #             elif(subhijo.tag=="clasificacion"):
-    #                 clasificacion=subhijo.text
-    #             elif (subhijo.tag=="stock"):
-    #                 stock=subhijo.text
-    #         juegos.append([nombre,plataforma,yearLanzamiento,clasificacion,stock])
-    #         print("Juego Guardado Exitosamente")
-        
-    #     elif(hijo.tag=="juegosMasVendidos"):
-    #         for subhijo in hijo:
-    #             if(subhijo.tag=="nombre"):
-    #                 nombre=subhijo.text
-    #             elif(subhijo.tag=="fechaUltimaCompra"):
-    #                 fechUltimaC=subhijo.text
-    #             elif(subhijo.tag=="copiasVendidas"):
-    #                 copiVendid=subhijo.text
-    #             elif(subhijo.tag=="stock"):
-    #                 stock=subhijo.text
-    #         juegosVendidos.append([nombre,fechUltimaC,copiVendid,stock])
-    #         print("Juego Mas Vendido Guardado Exitosamente")
+    print("\n ---------- Data Almacenada en las Listas Principales ------------\n")
+    print("\n###### Lista Clienetes######\n")
+    for i in listaclientes:
+        print(i)
+    print("\n###### Lista Mejores Clienetes######\n")
+    for i in listaMejoresClientes:
+        print(i)
+    print("\n###### Lista Juegos######\n")
+    for i in listaJuegos:
+        print(i)
+    print("\n###### Lista Juegos Mas Vendidos######\n")
+    for i in listaJuegosMasVendidos:
+        print(i)
+    
+    
 
-    # print("------------------------ Registro Completo Exito ----------------------------------------------")
+    print("\n---------Data para los Reportes a partir de aqui --------\n")
 
-    # for i in clientes:
-    #     print(i)
-    # print("###################### CLIENTES #######################")
+    #--------- Aqui valido el reporte Juegos Mas Vendidos
+    listaReporteJuegoMasVendido=[]
+    añoLanzamientoJuegoReporte=''
+    for x in range(len(listaJuegosMasVendidos)):
+        for y in range(len(listaJuegosMasVendidos[x])):
+            if(y==0):
+                nombreJuegoMasVendidoRepo=str(listaJuegosMasVendidos[x][y])
+                for x1 in range(len(listaJuegos)):
+                    for y1 in range(len(listaJuegos[x1])):
+                        if (y1==0):
+                            if(nombreJuegoMasVendidoRepo==str(listaJuegos[x1][y1])):
+                                print("Juego: "+nombreJuegoMasVendidoRepo+" igual a: "+str(listaJuegos[x1][y1]))
+                                #print("Juego Mas vendido si se Encuentra dentro de Juegos Mas Vendidos")
+                                print("Año Lanzamiento: "+str(listaJuegos[x1][2]))
+                                añoLanzamientoJuegoReporte=str(listaJuegos[x1][2])
+                                cantidadVendidaReporte=str(listaJuegosMasVendidos[x][2])
+                                listaReporteJuegoMasVendido.append([nombreJuegoMasVendidoRepo,añoLanzamientoJuegoReporte,cantidadVendidaReporte])
+                            else:
+                                print("Juego: "+nombreJuegoMasVendidoRepo+" No igual a: "+str(listaJuegos[x1][y1]))
+    
+   
+    #---------------- Aqui genero el reporte mejores Clientes
+    listaMejoresClientesReporte=[]
+    listaPosRepetidas=[]
+    for x in range(len(listaMejoresClientes)):
+        for y in range(len(listaMejoresClientes[x])):
+            if(y==0):
+                nombreMejorClienteReporte=str(listaMejoresClientes[x][y])
+                montoGastadoMejorCliente=float(listaMejoresClientes[x][3])
+                for x1 in range(len(listaMejoresClientes)):
+                    for y1 in range(len(listaMejoresClientes[x1])):
+                        if(y1==0):
+                            if(x1==x):
+                                print("Se esta Comparando el Mismo registro por lo cual se Salta")
+                            else:
+                                if(nombreMejorClienteReporte==str(listaMejoresClientes[x1][0])):
+                                    print("Existe Coincidencia en Datos")
+                                    montoGastadoMejorCliente=montoGastadoMejorCliente+float(listaMejoresClientes[x1][3])
+                                    listaPosRepetidas.append(x1)
+                                    #listaMejoresClientesReporte.append([nombreMejorClienteReporte,MontoTotal])
+                                else:
+                                    print("No se Genero Coincidencia")
+                listaMejoresClientesReporte.append([nombreMejorClienteReporte,str(montoGastadoMejorCliente)])
+    
+    listaMejoresClientesReporteFinal=[]
+    for i in listaMejoresClientesReporte:
+        if i not in listaMejoresClientesReporteFinal:
+            listaMejoresClientesReporteFinal.append(i)
+    tempListRepoMejoresClientes=sorted(listaMejoresClientesReporteFinal, key = lambda x:  float(x[1]))
+    listaMejoresClientesReporteFinalOrdenada=reversed(tempListRepoMejoresClientes)
+
+    #Aqui genero la data para el reporte Clasificacion
+    listaClasificaciones=[]
+    listaClasificacionesReporte=[]
+    listaTempClasificaciones=[]
+    tempOrdenadorCalificaciones=[]
+    for x in range(len(listaJuegos)):
+        for y in range(len(listaJuegos[x])):
+            listaTempClasificaciones.append(str(listaJuegos[x][3]))
+    listaClasificaciones=set(listaTempClasificaciones)
+    for i in listaClasificaciones:
+        print(i)
+    for i in listaClasificaciones:
+        contador=0
+        for x in range(len(listaJuegos)):
+            if (str(i)==str(listaJuegos[x][3])):
+                contador = contador+1
+        tempOrdenadorCalificaciones.append([str(i),str(contador)])
+    listaClasificacionesReporte=sorted(tempOrdenadorCalificaciones, key = lambda x:  int(x[1]))
+    listaClasificacionesReporteOrdenada=reversed(listaClasificacionesReporte)
 
 
-    # for i in mejoresClientes:
-    #     print(i)
-    # print("###################### MEJORES CLIENTES #######################")
+    
 
-    # for i in juegos:
-    #     print(i)
-    # print("###################### JUEGOS #######################")
 
-    # for i in juegosVendidos:
-    #     print(i)
-    # print("###################### MEJORES JUEGOS #######################")
+
+
+    print("\n-------------- Data Reportes Graficas ---------------\n")
+
+    print("\n-------------- Data Juegos Mas Vendidos --------------\n")
+    for i in listaReporteJuegoMasVendido:
+        print(i)
+    
+    print("\n------------ Datos Mejores Clientes ---------------\n")
+    for i in listaMejoresClientesReporteFinalOrdenada:
+        print(i)
+
+    print("\n------------ Datos Repo Clasificacion ---------------\n")    
+    for i in listaClasificacionesReporteOrdenada:
+        print(i)
 
     return Response(status=200)
+
+    
 
 @app.route('/returnXMLCompleto', methods = ['GET'])
 def return_XML():
